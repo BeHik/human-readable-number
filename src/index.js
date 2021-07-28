248a71570bc1c891e7241439f05a6786016e95fe
BeHik <behik3d@gmail.com>
2021-07-29 00:06:32 +0300

    refactor: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
+
+    const a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    const myNum = Number(number);
+    
+    if (isNaN(myNum)) return '';
+    if (myNum === 0) return 'zero';
+    numStr = (Math.trunc(myNum/100)>0) ? a[Math.trunc(myNum/100)] + 'hundred' : '';
+    numStr += (a[myNum % 100]===undefined) ? ' ' + b[Math.trunc((myNum % 100)/10)] + ' ' + a[myNum % 10] : ' ' + a[myNum % 100];
+    return numStr.trim()
 }
